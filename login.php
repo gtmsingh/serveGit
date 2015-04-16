40fb6967d53313b4f72c00541dbac6e765f00771
gtmsingh <gtm93singh@gmail.com>
2015-04-16 02:22:05 -0700

    Made login.php functional

--- login.php
+++ login.php
@@ -7,11 +7,22 @@ if( isVariablesSet([$_POST["email"], $_POST["password"]]) ) {
 	$email = $_POST["email"];
 	$password = $_POST["password"];
 
+	$query = "SELECT user_id, password FROM user_login WHERE email = '$email';";
+	$result = $link->query($query);
+	if(! $result) {
+		redirectWithError("login", "Server Error", "index.php");
+	} else {
+		if($result->num_rows != 1) {
+			redirectWithError("login", "Login details incorrect", "index.php");
+		} else {
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+			if($row["password"] == $password) {
 
-	die();
+				$_SESSION["uid"] = $row["user_id"];
+				header("Location: ./user/repo.php"); die();
+			}
+		}
+	}
 }
-$_SESSION["error"] = [
-	"login" => "Insuficient data supplied"
-];
-header("Location: index.php");
+redirectWithError("login", "Insuficient data supplied", "index.php");
 ?>
